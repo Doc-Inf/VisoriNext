5605d7725eac26baf19744d9e1ec69ba4a341cf0
leo0o7 <giardinoleonardo34@gmail.com>
2024-05-22 18:53:52 +0200

    added search by subject and topic

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,28 @@ import TextLG from "@/components/typhography/textLG";
 import Category from "@/components/videos/category";
 import { getClientVideos } from "@/constants/functions";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  console.log(searchParams);
+
+  const query = (function () {
+    let res = "";
+    if (searchParams.materia && searchParams.argomento) {
+      res = `?materia=${searchParams.materia}&argomento=${searchParams.argomento}`;
+    } else if (searchParams.argomento) {
+      res = `?argomento=${searchParams.argomento}`;
+    } else if (searchParams.materia) {
+      res = `?materia=${searchParams.materia}`;
+    }
+
+    return res;
+  })();
+
   const res = await fetch(
-    "https://www.itisvallauri.net/visori360/php/datiHome.php",
+    `https://www.itisvallauri.net/visori360/php/datiHome.php${query}`,
     { method: "GET", headers: { "Content-Type": "application/json" } }
   );
 
@@ -17,12 +36,16 @@ export default async function Home() {
   return (
     <>
       <Heading />
-      <SearchBar />
+      {/* @ts-ignore, TODO: fix */}
+      <SearchBar selected={searchParams} />
       {/* TODO: filter by category  and handle search */}
-      <TextLG className="mt-24 mb-2 text-center">Materia 1</TextLG>
-      <Category name="Argomento 1" videos={videos} />
-      <TextLG className="mt-12 mb-2 text-center">Materia 2</TextLG>
-      <Category name="Argomento 1" videos={videos} />
+      <TextLG className="mt-24 mb-2 text-center first-letter:capitalize">
+        {searchParams.materia || "Tutte le materie"}
+      </TextLG>
+      <Category
+        name={(searchParams.argomento as string) || "Tutti gli argomenti"}
+        videos={videos}
+      />
     </>
   );
 }
--- components/hero/search-bar.tsx
+++ components/hero/search-bar.tsx
@@ -1,6 +1,6 @@
+"use client";
 import { Button } from "../ui/button";
 import { Search } from "lucide-react";
-import { Input } from "../ui/input";
 import TextLG from "../typhography/textLG";
 import {
   Select,
@@ -11,13 +11,42 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function SearchBar() {
+export default function SearchBar({
+  selected,
+}: {
+  selected: { materia: string; argomento: string };
+}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const genQuery = (newParam: string, key: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (newParam !== "all") {
+      params.set(key, newParam);
+    } else {
+      params.delete(key);
+    }
+
+    return params.toString();
+  };
+  const handleRouting = (value: string, from: string) => {
+    const base = "/";
+    const query = genQuery(value, from);
+
+    router.push(`${base}?${query}`, {
+      scroll: false,
+    });
+  };
   return (
     <>
       <TextLG className="mb-4 text-center">Cerca nella videoteca</TextLG>
       <div className="m-auto space-x-2 items-center justify-center flex w-[80%] max-w-screen-md mb-8">
-        <Select>
+        <Select
+          onValueChange={(curr) => handleRouting(curr, "materia")}
+          value={selected.materia || "all"}
+        >
           <SelectTrigger className="w-[280px] rounded-xl shadow-md shadow-input dark:shadow-none">
             <SelectValue placeholder="Materia" className="text-white" />
             <p className="sr-only">Materia da cercare </p>
@@ -25,28 +54,41 @@ export default function SearchBar() {
           <SelectContent className="w-[280px] rounded-xl  shadow-input dark:shadow-none  pb-3">
             <SelectGroup className="px-2">
               <SelectLabel className="-px-1">Materia</SelectLabel>
-              <SelectItem className="text-md" value="mat1">
-                Materia 1
+              <SelectItem className="text-md" value="all">
+                Tutte
               </SelectItem>
-              <SelectItem className="text-md" value="mat2">
-                Materia 2
+              <SelectItem className="text-md" value="biologia">
+                Biologia
+              </SelectItem>
+              <SelectItem className="text-md" value="chimica">
+                Chimica
+              </SelectItem>
+              <SelectItem className="text-md" value="scienze della terra">
+                Scienze della Terra
               </SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
-        <Select>
+
+        <Select
+          onValueChange={(curr) => handleRouting(curr, "argomento")}
+          value={selected.argomento || "all"}
+        >
           <SelectTrigger className="w-[280px] rounded-xl shadow-md shadow-input dark:shadow-none">
             <SelectValue placeholder="Argomento" className="text-white" />
             <p className="sr-only">Argomento da cercare </p>
           </SelectTrigger>
           <SelectContent className="w-[280px] rounded-xl  shadow-input dark:shadow-none  pb-3">
             <SelectGroup className="px-2">
-              <SelectLabel className="-px-1">Materia</SelectLabel>
-              <SelectItem className="text-md" value="mat1">
-                Argomento 1
+              <SelectLabel className="-px-1">Argomento</SelectLabel>
+              <SelectItem className="text-md" value="all">
+                Tutti
               </SelectItem>
-              <SelectItem className="text-md" value="mat2">
-                Argomento 2
+              <SelectItem className="text-md" value="cellula">
+                Cellula
+              </SelectItem>
+              <SelectItem className="text-md" value="terremoto">
+                Terremoto
               </SelectItem>
             </SelectGroup>
           </SelectContent>
