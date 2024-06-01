c92d53c69fb328d31ab932b512a448563d509e79
leo0o7 <giardinoleonardo34@gmail.com>
2024-06-01 11:45:43 +0200

    created custom hooks for data fetching

--- components/hero/video-feed.tsx
+++ components/hero/video-feed.tsx
@@ -1,85 +1,22 @@
 "use client";
-import TextLG from "../typhography/textLG";
 import Category from "../videos/category";
 import SearchBar from "./search-bar";
-import { useEffect, useState } from "react";
-import { ClientVideo, ServerVideo } from "@/constants/types";
-import { getClientVideos } from "@/constants/functions";
+import { useState } from "react";
+import useHomeFetching from "@/lib/hooks/useHomeFetching";
 
+const getQuery = ({ topic, subject }: { subject: string; topic: string }) => {
+  const query = new URLSearchParams();
+  if (topic !== "all") query.append("argomento", topic);
+  if (subject !== "all") query.append("materia", subject);
+  return query.toString();
+};
 export default function VideoFeed() {
-  const [loading, setLoading] = useState(true);
-  const [videos, setVideos] = useState<ClientVideo[]>();
-  const [subjs, setSubjs] = useState<string[]>();
-  const [topics, setTopics] = useState<Map<string, Set<string>>>();
-
   const [selected, setSelected] = useState({
     subject: "all",
     topic: "all",
   });
-  useEffect(() => {
-    setLoading(true);
-    const searchParams = new URLSearchParams();
-    selected.subject !== "all" &&
-      searchParams.append("materia", selected.subject);
-    selected.topic !== "all" &&
-      searchParams.append("argomento", selected.topic);
-
-    fetch(`./php/datiHome.php?${searchParams.toString()}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) =>
-        res
-          .json()
-          .then((data: ServerVideo[]) => setVideos(getClientVideos(data)))
-      )
-      .catch((err) => console.log(err));
-
-    fetch("./php/datiHome.php?getMaterie", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) =>
-        res
-          .json()
-          .then((subjects) =>
-            setSubjs([
-              "all",
-              ...subjects.map((s: { nome: string; id: string }) =>
-                s.nome.toLowerCase()
-              ),
-            ])
-          )
-      )
-      .catch((err) => console.log(err));
-
-    fetch("./php/datiHome.php?getArgomenti", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) =>
-      res.json().then((data) => {
-        const map = new Map();
-        for (const { nomeArgomento, materia } of data) {
-          const lowerMateria = materia.toLowerCase();
-          const lowerArgomento = nomeArgomento.toLowerCase();
-          if (!map.has(lowerMateria)) {
-            map.set(lowerMateria, new Set([lowerArgomento]));
-            continue;
-          }
-          map.set(lowerMateria, map.get(lowerMateria).add(lowerArgomento));
-        }
-
-        setTopics(map);
-      })
-    );
-    setLoading(false);
-  }, [selected]);
+  const query = getQuery(selected);
+  const { videos, subjs, topics, loading, error } = useHomeFetching(query);
 
   return (
     <>
