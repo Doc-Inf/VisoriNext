d3d6665f3ff51c1d77f00436efd1cc12f5885805
leo0o7 <giardinoleonardo34@gmail.com>
2024-06-04 11:33:08 +0200

    cleaned up branch

--- components/typhography/textSM.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function TextSM({ children }: { children: ReactNode }) {
-  return (
-    <h4 className="text-xl font-semibold tracking-tight scroll-m-20">
-      {children}
-    </h4>
-  );
-}
